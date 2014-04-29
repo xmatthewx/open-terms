32f873363bd2e9b4194781199fb4e2f6fe3aba92
xmatthewx <mwillse@gmail.com>
2014-04-29 12:01:01 -0700

    improving notices

--- openterms-addon/data/openterms.js
+++ openterms-addon/data/openterms.js
@@ -18,14 +18,14 @@ ot.html = '<section class="ot-container">'
 
 ot.notices = [
 	'<h1><span>SITENAME</span> can change their terms without telling you.</h1><p>Their last switcheroo was on March 25, 2014.<br>View the changes on <a href="http://tosback.org">tosback.org</a>.</p>',
-	'<h1><span>SITENAME</span> can share your personal info without telling you.</h1><p>Don\'t like that? Let them know. </p>',
+	'<h1><span>SITENAME</span> can share your personal info without telling you.</h1><p>Don\'t like that? <a class="showForm" href="#">Let them know</a>.<form class="hidden"><textarea></textarea><br><button>Send comment</button></form> </p>',
 	'<h1>Do you really agree?</h1><p><input type="checkbox" > I can\'t read all these terms and conditions.<br><input type="checkbox"> I agree because I feel like I don\'t have much choice.</p><p><button>Submit</button></p>',
 	'<h1><span>SITENAME</span> has a perpetual license on anything you publish.</h1><p>Why? Because they said so. Learn about <a href="http://tosdr.org/#SITENAME">what else they say</a>.</p>',
 	'<h1><span>SITENAME</span> got a poor grade for their terms of service.</h1><p>Luckily, they\'re not the only game in town. Browse options on <a href="http://alternativeto.net/software/SITENAME/">alterativeto.net</a></p>',
 	'<h1>You have agreed to 242,280 words in legal terms.</h1><p>View and <a href="#">manage your Open Terms</a>.  </p>',
 	'<h1>Is this sign up page missing a link to the legal terms?</h1><p><button>Yes</button> <button>No</button></p>',
-	'<h1>What? You don\'t like scrolling through 600 lines of legalese while on the go?</h1><p>Ask <span>SITENAME</span> to join Common Terms</p><p><button>Open the Web</button></p>',
-	'<h1>pur&middot;su&middot;ant</h1> <p>adverb</p> <p>1. in accordance with (a law or a legal document or resolution).</p> <p><button>Open the Web</button></p>',
+	'<h1>What? You don\'t like scrolling through 600 lines of legalese while on the go?</h1><p>Ask <span>SITENAME</span> to join <a href="http://www.commonterms.net/">Common Terms</a></p><p><button>Open the Web</button></p>',
+	'<h1>pur&middot;su&middot;ant</h1> <p>adverb</p> <p>1. in accordance with (a law or a legal document or resolution).</p><p>Bleh. Ask SITENAME to use <a href="http://www.commonterms.net/">Common Terms</a>.</p> <p><button>Open the Web</button></p>',
 ];
 
 ot.getNotice = function (xyz) {
